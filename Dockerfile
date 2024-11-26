566d162ab5fd2c4486eeafd48f74ce38de9aebbe
yubingjiaocn <yubingjiaocn@gmail.com>
2024-11-26 09:27:12 +0000

    Update image

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ RUN mvn -B clean package -Dmaven.test.skip=true
 FROM public.ecr.aws/amazoncorretto/amazoncorretto:8-al2023-jre AS runner
 WORKDIR /app
 COPY --from=builder /app/target/demo-V1024.jar /app
-CMD ["java -jar demo-V1024.jar"]
+EXPOSE 8989
+CMD ["/bin/sh", "-c", "java -jar /app/demo-V1024.jar"]
--- src/main/java/com/sprint/jacoco/jacocodemo/com/controller/jacoco.java
+++ src/main/java/com/sprint/jacoco/jacocodemo/com/controller/jacoco.java
@@ -2,39 +2,51 @@ package com.sprint.jacoco.jacocodemo.com.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class jacoco {
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello Word";
-    }
 
-    @RequestMapping("/index")
-    public String login() {
-        return "欢迎来到登录首页！";
-    }
+    @RequestMapping(value = "/", produces = "application/json")
+    @ResponseBody
+    public Map<String, Object> hello(HttpServletRequest request, @RequestBody(required = false) String body) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            // System Information
+            InetAddress addr = InetAddress.getLocalHost();
+            Runtime runtime = Runtime.getRuntime();
 
+            Map<String, Object> systemInfo = new HashMap<>();
+            systemInfo.put("hostname", addr.getHostName());
+            systemInfo.put("ip", addr.getHostAddress());
+            systemInfo.put("cpuArchitecture", System.getProperty("os.arch"));
+            systemInfo.put("processorCount", runtime.availableProcessors());
+            systemInfo.put("osName", System.getProperty("os.name"));
+            systemInfo.put("osVersion", System.getProperty("os.version"));
 
-    @RequestMapping("/b")
-    public String contrastB() {
-        int a = 5;
-        int b = 5;
-        if (a > b) {
-            return "a<b";
-        } else {
-            return "b>a";
+            // Request Headers
+            Map<String, String> headers = new HashMap<>();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                headers.put(headerName, request.getHeader(headerName));
+            }
+
+            response.put("systemInfo", systemInfo);
+            response.put("headers", headers);
+            response.put("body", body != null ? body : "");
+
+        } catch (Exception e) {
+            response.put("error", e.getMessage());
         }
+
+        return response;
     }
 
-    @RequestMapping("/a")
-    public String contrastA() {
-        int a = 5;
-        int b = 1;
-        if (a > b) {
-            return "a>b";
-        } else {
-            return "b>a";
-        }
-    }
 }
